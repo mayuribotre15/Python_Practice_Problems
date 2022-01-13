1f779b436ac88c31d9d3a93bc0bb29caba090169
Mayuri Botre <82947982+mayuribotre15@users.noreply.github.com>
2022-01-13 12:36:43 +0530

    Update Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -4,6 +4,6 @@ for i in range(int(input())):
     #Enter integer to calculate factorial
     num = int(input())
     fact = 1
-    for j in range(1,num+1):
+    for j in range(1,num+1):               # instead of for loop we can use while loop
         fact *= j
     print(fact)
